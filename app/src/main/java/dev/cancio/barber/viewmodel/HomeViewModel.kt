8f8e04b102cd139a4a426bee4e6e84b990959ce1
Filipe Câncio <filipe.cancio@gmail.com>
2022-10-31 20:55:57 -0300

    :tada: Criando Home View Model

--- app/src/main/java/dev/cancio/barber/viewmodel/HomeViewModel.kt
+++ app/src/main/java/dev/cancio/barber/viewmodel/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package dev.cancio.barber.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -8,7 +9,7 @@ class HomeViewModel(): ViewModel() {
 
     fun onClickButton(scope: CoroutineScope){
         scope.launch {
-
+            Log.e("Message","foi pivete")
         }
     }
 }
