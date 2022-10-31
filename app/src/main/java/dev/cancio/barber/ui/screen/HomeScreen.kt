90d100a6cae2bdf0674f4f8c41e4e2cfc18d30c9
Filipe Câncio <filipe.cancio@gmail.com>
2022-10-31 20:50:23 -0300

    :tada: Criando Home Screen

--- app/src/main/java/dev/cancio/barber/ui/screen/HomeScreen.kt
+++ app/src/main/java/dev/cancio/barber/ui/screen/HomeScreen.kt
@@ -1,18 +1,30 @@
 package dev.cancio.barber.ui.screen
 
+import android.text.style.ClickableSpan
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
     Box(
         modifier = Modifier.fillMaxWidth()
     ) {
-        Box(){
+        Button(
+            modifier = Modifier
+                .align(Alignment.Center),
+            onClick = { /*TODO*/ }
+        ) {
             Text(text = "Aperte")
         }
     }
