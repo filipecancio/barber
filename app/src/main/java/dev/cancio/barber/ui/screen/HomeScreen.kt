3665353b2ded0cbbf0e35bda23d96fa00e16084f
Filipe Câncio <filipe.cancio@gmail.com>
2022-10-31 21:15:14 -0300

    :tada: Criando Status

--- app/src/main/java/dev/cancio/barber/ui/screen/HomeScreen.kt
+++ app/src/main/java/dev/cancio/barber/ui/screen/HomeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +17,7 @@ import dev.cancio.barber.viewmodel.HomeViewModel
 fun HomeScreen() {
     val scope = rememberCoroutineScope()
     val viewModel = HomeViewModel()
+    val state = remember{ mutableStateOf(HearState.Not) }
 
     Box(
         modifier = Modifier.fillMaxWidth()
@@ -22,11 +25,19 @@ fun HomeScreen() {
         Button(
             modifier = Modifier
                 .align(Alignment.Center),
-            onClick = { viewModel.onClickButton(scope) }
+            onClick = { state.value = viewModel.onClickButton(scope) }
         ) {
-            Text(text = "Aperte")
+            Text(text = when(state.value){
+                HearState.Yes -> "Ouvindo"
+                else -> "Aperte"
+            })
         }
     }
+}
+
+enum class HearState(){
+    Yes(),
+    Not()
 }
 
 @Preview(
--- app/src/main/java/dev/cancio/barber/viewmodel/HomeViewModel.kt
+++ app/src/main/java/dev/cancio/barber/viewmodel/HomeViewModel.kt
@@ -2,14 +2,16 @@ package dev.cancio.barber.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import dev.cancio.barber.ui.screen.HearState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class HomeViewModel(): ViewModel() {
 
-    fun onClickButton(scope: CoroutineScope){
+    fun onClickButton(scope: CoroutineScope): HearState{
         scope.launch {
             Log.e("Message","foi pivete")
         }
+        return HearState.Yes
     }
 }
